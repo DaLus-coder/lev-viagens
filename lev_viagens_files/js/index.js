2acbc30685827970ce4fdf132ce2f3ef7f24da4a
DaLus-coder <142961966+DaLus-coder@users.noreply.github.com>
2026-06-06 21:07:57 -0300

    comit move more features

---
+++ lev_viagens_files/js/index.js
@@ -0,0 +1,70 @@
+
+const track = document.querySelector(".carousel-track");
+
+const cards = document.querySelectorAll(".card");
+
+const dots = document.querySelectorAll(".dot");
+
+const next = document.querySelector(".right");
+const prev = document.querySelector(".left");
+
+let current = 0;
+
+function updateCarousel() {
+
+    track.style.transform =
+        `translateX(-${current * 100}%)`;
+
+    dots.forEach(dot =>
+        dot.classList.remove("active")
+    );
+
+    dots[current].classList.add("active");
+}
+
+next.addEventListener("click", () => {
+
+    current++;
+
+    if (current >= cards.length) {
+        current = 0;
+    }
+
+    updateCarousel();
+});
+
+prev.addEventListener("click", () => {
+
+    current--;
+
+    if (current < 0) {
+        current = cards.length - 1;
+    }
+
+    updateCarousel();
+});
+
+/* Auto-play */
+
+setInterval(() => {
+
+    current++;
+
+    if (current >= cards.length) {
+        current = 0;
+    }
+
+    updateCarousel();
+
+}, 5000);
+
+updateCarousel();
+
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuBtn.addEventListener("click", function () {
+
+    mobileMenu.classList.toggle("active");
+
+});
