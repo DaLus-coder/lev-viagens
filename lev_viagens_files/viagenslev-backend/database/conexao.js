d52b3d3359207487ff42e4774aabe104e57d64ae
DaLus-coder <142961966+DaLus-coder@users.noreply.github.com>
2026-07-22 23:43:14 -0300

    Update conexao.js

--- lev_viagens_files/viagenslev-backend/database/conexao.js
+++ lev_viagens_files/viagenslev-backend/database/conexao.js
@@ -10,7 +10,14 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: Number(process.env.DB_PORT),
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 10000
 });
 
 module.exports = pool;
